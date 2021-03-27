53c88ac96bfc9313bab6f44ba9ef7c007abe7938
x14102004x <marcosproenca144@gmail.com>
2021-03-26 23:45:20 -0300

    feat: appointments route was created

---
+++ server/src/routes/appointments.router.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.post("/", (request, response) => {
+    
+
+});
+
+export default appointmentsRouter;
