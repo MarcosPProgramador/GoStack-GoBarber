bfd5d6f6bff4da400e904e48187585e86a3567c2
x14102004x <marcosproenca144@gmail.com>
2021-04-02 14:51:13 -0300

    feat: global exception handler added

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,39 @@
 import "reflect-metadata";
 import "./database/connection";
 
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+
+import uploadConfig from "./config/upload";
 import cors from "cors";
-import express from "express";
 import router from "./routes/router";
+import AppError from "./errors/AppError";
 
 const app = express();
 
 app.use(express.json());
+app.use("/static", express.static(uploadConfig.directory));
 app.use(cors());
 app.use(router);
+
+/**
+ * Global Exception Handler
+ */
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: "error",
+        message: err.message,
+      });
+    }
+    console.error(err);
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
+);
 
 export default app;
