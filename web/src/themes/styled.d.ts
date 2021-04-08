863ba079630369b8488c1eceb2ec61d4ea3075b6
x14102004x <marcosproenca144@gmail.com>
2021-04-08 18:58:04 -0300

    feat: typing added themes

---
+++ web/src/themes/styled.d.ts
@@ -0,0 +1,17 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    title: 'light' | 'dark'
+    colors: {
+      shape: '#3E3B47'
+      hard: '#666360'
+      sub: '#999591'
+      orange: '#FF9000'
+      medium: '#28262E'
+      background: '#312E38'
+      inputs: '#232129'
+      text: '#f0f8ff'
+    }
+  }
+}
