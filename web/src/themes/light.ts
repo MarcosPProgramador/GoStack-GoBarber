215c510a02c78e1b750531b5fab449a0d6c262f7
x14102004x <marcosproenca144@gmail.com>
2021-04-08 18:54:48 -0300

    feat: dark and light colors added

---
+++ web/src/themes/light.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+export default {
+  title: 'light',
+  colors: {
+    shape: '#3E3B47',
+    hard: '#666360',
+    sub: '#999591',
+    orange: '#FF9000',
+    medium: '#28262E',
+    background: '#312E38',
+    inputs: '#232129',
+    text: '#f0f8ff',
+  }
+} as DefaultTheme
