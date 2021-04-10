48484560152558c9d7f28234d1407440e9999448
x14102004x <marcosproenca144@gmail.com>
2021-04-10 10:13:50 -0300

    refactor: updated upload settings

--- server/src/config/upload.ts
+++ server/src/config/upload.ts
@@ -1,18 +1,20 @@
-import multer from "multer";
-import path from "path";
-import crypto from "crypto";
 
-const tmpFolder = path.resolve(__dirname, "..", "..", "tmp");
+
+import crypto from 'crypto'
+import multer from 'multer'
+import path from 'path'
+
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp')
 
 export default {
   directory: tmpFolder,
   storage: multer.diskStorage({
     destination: tmpFolder,
-    filename: (req, file, callback) => {
-      const fileHash = crypto.randomBytes(10).toString("hex");
-      const fileName = `${fileHash}-${file.originalname}`;
+    filename: (request, file, callback) => {
+      const fileHash = crypto.randomBytes(10).toString('hex')
+      const fileName = `${fileHash}-${file.originalname}`
 
-      callback(null, fileName);
+      callback(null, fileName)
     },
   }),
-};
+}
