6eb52bae40e87fe848f2f4f81db7bc80ff16d139
x14102004x <marcosproenca144@gmail.com>
2021-04-12 12:24:47 -0300

    feat: service api added

---
+++ web/src/components/ScrollBar/index.tsx
@@ -0,0 +1,15 @@
+import React, { HtmlHTMLAttributes } from 'react'
+import { Container } from './styles'
+interface IScrollProps extends HtmlHTMLAttributes<HTMLDivElement> {
+  colors: {
+    primary: string,
+    secondary: string
+  }
+}
+const ScrollBar: React.FC<IScrollProps> = ({ children, colors }) => {
+  return (
+    <Container colors={colors}>{children}</Container>
+  )
+}
+
+export default ScrollBar
---
+++ web/src/components/ScrollBar/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+interface IScrollProps {
+  colors: {
+    primary: string,
+    secondary: string
+  }
+}
+export const Container = styled.div<IScrollProps>`
+  &::-webkit-scrollbar {
+    width: 16px;
+    height: 16px;
+    background-color: ${props => props.colors.primary};
+  }
+  &::-webkit-scrollbar-thumb {
+    border-right-width: 3px;
+    border-left-width: 3px;
+    border-style: solid;
+    border-color: ${props => props.colors.primary};
+
+    background-color: ${props => props.colors.secondary};
+    border-radius: 50rem;
+  }
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+`
+
