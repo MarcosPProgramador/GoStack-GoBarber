58bbfce5cace1abc031115707c15185ba9f94c8e
x14102004x <marcosproenca144@gmail.com>
2021-04-16 14:06:53 -0300

    fix: png image import resolved

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,13 +1,20 @@
+import 'react-native-gesture-handler';
+import { useFonts } from 'expo-font';
 import React from 'react';
 import { StatusBar, View } from 'react-native';
-import 'react-native-gesture-handler';
 import AppRoutes from './routes/app.routes';
 
 
 const App: React.FC = () => {
+  const [loaded] = useFonts({
+    'RobotoSlab-Medium': require('../assets/fonts/RobotoSlab-Medium.ttf'),
+    'RobotoSlab-Regular': require('../assets/fonts/RobotoSlab-Regular.ttf'),
+  });
+  if (!loaded) return null
+
   return (
     <>
-      <StatusBar translucent barStyle={'light-content'} />
+      <StatusBar backgroundColor={'#312e38'} barStyle={'light-content'} />
       <View style={{ flex: 1, backgroundColor: '#312e38' }}>
         <AppRoutes />
       </View>
