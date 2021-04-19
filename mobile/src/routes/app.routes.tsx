07b094ad0eefbed263a4dea753be271941a2aff3
x14102004x <marcosproenca144@gmail.com>
2021-04-19 20:52:05 -0300

    feat: application routes created

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,24 +1,20 @@
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-import SignIn from '../pages/SignIn';
-import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
 
-const Stack = createStackNavigator();
+const App = createStackNavigator();
 
 const AppRoutes: React.FC = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-          cardStyle: { backgroundColor: '#312e38' },
-        }}
-        initialRouteName="SignIn">
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <App.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: '#312e38',
+        },
+      }}>
+      <App.Screen name="Dashboard" component={Dashboard} />
+    </App.Navigator>
   );
 };
 
