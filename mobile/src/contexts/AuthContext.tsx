9e74c364195fd8c62564ab6cc56799756e8c7910
x14102004x <marcosproenca144@gmail.com>
2021-04-19 20:47:24 -0300

    feat: using asynchronous storage and added signin

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,25 +1,63 @@
-import React, { createContext, useCallback, useState } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import React, { createContext, useCallback, useEffect, useState } from 'react';
+import api from '../services/api';
 
-interface SignInFormData {
+interface ISignInFormData {
   email: string;
   password: string;
 }
+interface IAuthState {
+  token: string;
+  user: object;
+}
 interface IAuthContext {
-  SignIn: (data: SignInFormData) => void;
-  SignOut: () => void;
+  signIn: (data: ISignInFormData) => Promise<void>;
+  signOut: () => void;
+  user: object;
+  loading: boolean;
 }
 const AuthContext = createContext<IAuthContext>({} as IAuthContext);
 
 const AuthProvider: React.FC = ({ children }) => {
-  const [user, setUser] = useState();
-  const SignIn = useCallback(async (data: SignInFormData) => {
-    console.log(data);
+  const [data, setData] = useState({} as IAuthState);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    async function loadStoragedData() {
+      const [user, token] = await AsyncStorage.multiGet(['@GoBarber:user', '@GoBarber:token']);
+
+      if (token[1] && user[1]) {
+        setData({ token: token[1], user: JSON.parse(user[1]) });
+      }
+      setLoading(false);
+    }
+    loadStoragedData();
   }, []);
-  const SignOut = useCallback(() => {
-    console.log('out');
+  const signIn = useCallback(async (formData: ISignInFormData) => {
+    const {
+      data: { token, user },
+    } = await api.post('/sessions', formData);
+
+    await AsyncStorage.multiSet([
+      ['@GoBarber:token', token],
+      ['@GoBarber:user', JSON.stringify(user)],
+    ]);
+
+    setData({
+      user,
+      token,
+    });
   }, []);
 
-  return <AuthContext.Provider value={{ SignIn, SignOut }}>{children}</AuthContext.Provider>;
+  const signOut = useCallback(async () => {
+    await AsyncStorage.multiRemove(['@GoBarber:token', '@GoBarber:user']);
+    setData({} as IAuthState);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ loading, user: data.user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export { IAuthContext, AuthContext };
