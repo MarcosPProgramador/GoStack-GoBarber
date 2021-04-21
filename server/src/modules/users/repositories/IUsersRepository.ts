cbfea2d9d0fdde2a89c3ff3e3456370317756540
x14102004x <marcosproenca144@gmail.com>
2021-04-21 16:23:53 -0300

    refactor: liskov substitution principle added

---
+++ server/src/modules/users/repositories/IUsersRepository.ts
@@ -0,0 +1,10 @@
+import ICreateUserDTO from "../dtos/ICreateUserDTO";
+import User from "../infra/typeorm/entities/User";
+
+export default interface IUsersRepository {
+  findByEmail(email: string): Promise<User | undefined>
+  findById(id: string | number): Promise<User | undefined>
+  create(userData: ICreateUserDTO): Promise<User>
+  save(user: User): Promise<User>
+
+}
