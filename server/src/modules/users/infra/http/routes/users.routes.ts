8d55683abbf50efa72ebb6a13991ee8c8a16a583
x14102004x <marcosproenca144@gmail.com>
2021-04-22 17:12:05 -0300

    refactor: users controller changed to user avatar controller

--- server/src/modules/users/infra/http/routes/users.routes.ts
+++ server/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,15 +1,17 @@
 import uploadConfig from '@config/upload'
 import { Router } from 'express'
 import multer from 'multer'
+import UserAvatarController from '../controllers/UserAvatarController'
 import UsersController from '../controllers/UsersController'
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
 
 const usersRouter = Router()
 const usersController = new UsersController()
+const userAvatarController = new UserAvatarController()
 
 const upload = multer(uploadConfig)
 
-usersRouter.post('/', usersController.store)
-usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), usersController.updateAvatar)
+usersRouter.post('/', usersController.create)
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), userAvatarController.update)
 
 export default usersRouter
