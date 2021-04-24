c4efee008d88ef7ed382111c4365a95ef0450b10
x14102004x <marcosproenca144@gmail.com>
2021-04-24 15:28:02 -0300

    feat: upload directory was added

--- server/src/config/upload.ts
+++ server/src/config/upload.ts
@@ -5,13 +5,14 @@ import path from 'path'
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp')
 
 export default {
-  directory: tmpFolder,
+  tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
   storage: multer.diskStorage({
     destination: tmpFolder,
     filename: (request, file, callback) => {
       const hash = crypto.randomBytes(10).toString('hex')
       const originalName = file.originalname.replace(' ', '-')
-      
+
       const fileName = `${hash}-${originalName}`
 
       callback(null, fileName)
