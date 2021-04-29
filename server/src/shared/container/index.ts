1a29d4f0294c0447d349c2ef7385b16c12cdd7ac
x14102004x <marcosproenca144@gmail.com>
2021-04-29 19:05:40 -0300

    feat: dependency injection tsyringe container added

--- server/src/shared/container/index.ts
+++ server/src/shared/container/index.ts
@@ -1,9 +1,16 @@
 import { container } from 'tsyringe'
+
+import '@modules/users/providers'
+import './providers'
+
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository'
 import IUsersRepository from '@modules/users/repositories/IUsersRepository'
 
+
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository'
+
+
 
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository)
 container.registerSingleton<IAppointmentsRepository>('AppointmentsRepository', AppointmentsRepository)
