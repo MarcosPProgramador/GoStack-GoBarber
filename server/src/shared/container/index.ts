75a9697be03e4947e82dfa27251889ca0f3d7a86
x14102004x <marcosproenca144@gmail.com>
2021-05-06 17:51:58 -0300

    feat: user-tokens-repository was registered

--- server/src/shared/container/index.ts
+++ server/src/shared/container/index.ts
@@ -9,8 +9,11 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository'
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository'
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository'
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository'
 
 
 
+container.registerSingleton<IUserTokensRepository>('UserTokensRepository', UserTokensRepository)
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository)
 container.registerSingleton<IAppointmentsRepository>('AppointmentsRepository', AppointmentsRepository)
