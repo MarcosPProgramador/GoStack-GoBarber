ebdcef79eaaf9d11bf3865b545d171e6881fe841
x14102004x <marcosproenca144@gmail.com>
2021-05-07 19:24:47 -0300

    feat: providers-controller layer was created

---
+++ server/src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -0,0 +1,17 @@
+import { Response, Request } from 'express'
+import ListProvidersService from '@modules/appointments/services/ListProvidersService'
+import { container } from 'tsyringe'
+
+export default class ProvidersController {
+  async index(request: Request, response: Response): Promise<Response> {
+
+    const user_id = request.user.id
+
+    const listProviders = container.resolve(ListProvidersService)
+
+    const providers = await listProviders.execute({
+      user_id
+    })
+    return response.status(200).json(providers)
+  }
+}
