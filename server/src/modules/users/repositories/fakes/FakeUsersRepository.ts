34628413f926771f92bebdb5206412f35b4d9867
x14102004x <marcosproenca144@gmail.com>
2021-05-07 19:35:48 -0300

    test: fake find-all-providers method was added

--- server/src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ server/src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -1,10 +1,19 @@
 import ICreateUserDTO from "@modules/users/dtos/ICreateUserDTO";
+import IFindAllProvidersDTO from "@modules/users/dtos/IFindAllProvidersDTO";
 import User from "@modules/users/infra/typeorm/entities/User";
 import { v4 as uuid } from 'uuid';
 import IUsersRepository from "../IUsersRepository";
 
 class FakeUsersRepository implements IUsersRepository {
   private users: User[] = []
+  async findAllProviders({ except_user_id }: IFindAllProvidersDTO): Promise<User[]> {
+    let { users } = this
+
+    if(except_user_id) {
+      users = this.users.filter(user => user.id !== except_user_id)
+    }
+    return users
+  }
   async findByEmail(email: string): Promise<User | undefined> {
     const findUserByEmail = this.users.find(user => user.email === email)
 
