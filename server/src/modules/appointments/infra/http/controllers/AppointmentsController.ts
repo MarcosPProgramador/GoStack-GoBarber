a4e9e64eac62202ad46344cd725149ac52459b63
x14102004x <marcosproenca144@gmail.com>
2021-05-12 17:55:46 -0300

    refactor: the index method was removed from of class

--- server/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ server/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -4,25 +4,20 @@ import { Request, Response } from "express"
 import { container } from "tsyringe"
 
 class AppointmentsController {
-  async index(request: Request, response: Response)/* : Promise<Response> */ {
-    /*
-      const appointmentsRepository = new AppointmentsRepository()
-      const appointments = await appointmentsRepository.find()
-      return response.status(200).json(appointments)
-    */
-  }
   async create(request: Request, response: Response): Promise<Response> {
-
     const { provider_id, date } = request.body
+    const user_id = request.user.id
+
     const parsedDate = parseISO(date)
 
     const createAppointmentService = container.resolve(CreateAppointmentService)
     const appointment = await createAppointmentService.execute({
       provider_id,
+      user_id,
       date: parsedDate,
     })
 
-    return response.status(200).json(appointment)
+    return response.status(201).json(appointment)
   }
 
 }
