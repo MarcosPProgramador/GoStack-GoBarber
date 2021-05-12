752db76676c0760c29607292a14f1087e7187309
x14102004x <marcosproenca144@gmail.com>
2021-05-12 17:56:59 -0300

    feat: new controller was created with index method

---
+++ server/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -0,0 +1,20 @@
+import ListProviderAppointmentsService from "@modules/appointments/services/ListProviderAppointmentsService"
+import { Request, Response } from "express"
+import { container } from "tsyringe"
+
+export default class ProviderAppointmentsController {
+  async index(request: Request, response: Response) : Promise<Response>  {
+    const provider_id = request.user.id
+    const { day, month, year } = request.body
+
+    const listProviderAppointments = container.resolve(ListProviderAppointmentsService)
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id,
+      day,
+      month,
+      year
+    })
+    return response.status(200).json(appointments)
+  }
+}
