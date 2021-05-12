3788124d702ec14d945ee9097e01bde5834bb356
x14102004x <marcosproenca144@gmail.com>
2021-05-12 18:01:56 -0300

    feat: get route created to get all appointments from the provider

--- server/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ server/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,13 +1,15 @@
 import { ensureAuthenticated } from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 import { Router } from 'express'
 import AppointmentsController from '../controllers/AppointmentsController'
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController'
 
 const appointmentsRouter = Router()
 const appointmentsController = new AppointmentsController()
+const providerAppointmentsController = new ProviderAppointmentsController()
 
 appointmentsRouter.use(ensureAuthenticated)
 
-appointmentsRouter.get('/', appointmentsController.index)
+appointmentsRouter.get('/me', providerAppointmentsController.index)
 appointmentsRouter.post('/', appointmentsController.create)
 
 export default appointmentsRouter
