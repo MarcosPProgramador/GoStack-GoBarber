f9b3d158396b2c05f1fa9e496fc969247314683b
x14102004x <marcosproenca144@gmail.com>
2021-05-12 18:26:29 -0300

    feat: month availability and day availibility routes were added

--- server/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ server/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,13 +1,19 @@
 import { ensureAuthenticated } from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 import { Router } from 'express'
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController'
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController'
 import ProvidersController from '../controllers/ProvidersController'
 
 const providersRouter = Router()
 
 const providersController = new ProvidersController()
+const providerDayAvailabilityController = new ProviderDayAvailabilityController()
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController()
 
 providersRouter.use(ensureAuthenticated)
 
 providersRouter.get('/', providersController.index)
+providersRouter.get('/:provider_id/month-availability', providerMonthAvailabilityController.index)
+providersRouter.get('/:provider_id/day-availability', providerDayAvailabilityController.index)
 
 export default providersRouter
