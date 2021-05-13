682f6a7fe64bfd7aad1616f9fb4edd565d9d1d36
x14102004x <marcosproenca144@gmail.com>
2021-05-13 18:50:15 -0300

    feat: more business logic were added

--- server/src/modules/appointments/services/CreateAppointmentService.ts
+++ server/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,11 +1,14 @@
 import AppError from '@shared/errors/AppError'
-import { startOfHour } from 'date-fns'
+import { getHours, isBefore, startOfHour,format } from 'date-fns'
 import { inject, injectable } from 'tsyringe'
 import Appointment from '../infra/typeorm/entities/Appointment'
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository'
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository'
+import IUsersRepository from '@modules/users/repositories/IUsersRepository'
 
 interface RequestDTO {
   provider_id: string
+  user_id: string
   date: Date
 }
 /**
@@ -24,22 +27,49 @@ class CreateAppointmentService {
 
   constructor(
     @inject('AppointmentsRepository')
-    private appointmentsRepository: IAppointmentsRepository
+    private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('NotificationsRepository')
+    private notificationsRepository: INotificationsRepository
   ) { }
 
   /**
    * execute
    */
 
-  async execute({ provider_id, date }: RequestDTO): Promise<Appointment> {
+  async execute({ provider_id, user_id, date }: RequestDTO): Promise<Appointment> {
     const appointmentDate = startOfHour(date)
 
+    if(isBefore(appointmentDate, Date.now()))
+      throw new AppError(`you can't create an appointment at a past date.`)
+
+    if(user_id === provider_id)
+      throw new AppError(`you can't create an appointment with yourself.`)
+
+    if(getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17)
+      throw new AppError('you can only create an appointment between at 8am and 5pm.')
+
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(appointmentDate)
-    if (findAppointmentInSameDate) throw new AppError('This appointment is already booked')
+    if (findAppointmentInSameDate) throw new AppError('this appointment is already booked.')
 
     const appointment = await this.appointmentsRepository.create({
       provider_id,
+      user_id,
       date: appointmentDate,
+    })
+
+    const user = await this.usersRepository.findById(user_id)
+
+    if(!user) throw new AppError('user does not exist.')
+
+    const dateFormatted = format(appointmentDate, `dd/MM/yyyy 'às' HH:mm'h'`)
+
+    await this.notificationsRepository.create({
+      recipient_id: provider_id,
+      content: `${user.name} marcou um novo agendamento para o dia ${dateFormatted}`
     })
 
     return appointment
