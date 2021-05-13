640ed210632db5466aef9690dd01fe572d8d86f7
x14102004x <marcosproenca144@gmail.com>
2021-05-13 18:45:56 -0300

    feat: celebrate validation middleware was added to the route

--- server/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ server/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,5 +1,6 @@
 import { ensureAuthenticated } from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 import { Router } from 'express'
+import {Segments, Joi, celebrate} from 'celebrate'
 import AppointmentsController from '../controllers/AppointmentsController'
 import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController'
 
@@ -9,7 +10,12 @@ const providerAppointmentsController = new ProviderAppointmentsController()
 
 appointmentsRouter.use(ensureAuthenticated)
 
-appointmentsRouter.get('/me', providerAppointmentsController.index)
-appointmentsRouter.post('/', appointmentsController.create)
+appointmentsRouter.get('/me',providerAppointmentsController.index)
+appointmentsRouter.post('/', celebrate({
+  [Segments.BODY]: {
+    provider_id: Joi.string().uuid().required(),
+    date: Joi.string()
+  }
+}), appointmentsController.create)
 
 export default appointmentsRouter
