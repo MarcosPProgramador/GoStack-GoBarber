2b2535e2f4a356d590c5134561dccfc4b44cd9b4
x14102004x <marcosproenca144@gmail.com>
2021-05-14 18:59:24 -0300

    test: the fake-notifications-repository was created to the tests

---
+++ server/src/modules/notifications/repositories/fakes/FakeNotificationsRepository.ts
@@ -0,0 +1,29 @@
+import { ObjectID } from 'mongodb'
+
+import {v4 as uuid} from 'uuid'
+import ICreateNotificationDTO from "@modules/notifications/dtos/ICreateNotificationDTO";
+import Notification from "@modules/notifications/infra/typeorm/schemas/Notification";
+import INotificationRepository from "../INotificationsRepository";
+
+export default class FakeNotificationsRepository implements INotificationRepository{
+  private notifications: Notification[] = []
+
+  async create({content, recipient_id}: ICreateNotificationDTO): Promise<Notification> {
+    const notification = new Notification()
+
+    Object.assign(notification, {
+      id: new ObjectID(),
+      content,
+      recipient_id,
+      read: false,
+      created_at: new Date(),
+      updated_at: new Date()
+    })
+
+    this.notifications.push(notification)
+
+    return notification
+  }
+
+
+}
