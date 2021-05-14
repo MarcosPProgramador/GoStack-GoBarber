72195791700286e0555bb144b20d0a147472da0a
x14102004x <marcosproenca144@gmail.com>
2021-05-14 19:43:56 -0300

    refactor: the celebrate validation middleware was added in sessions

--- server/src/modules/users/infra/http/routes/sessions.routes.ts
+++ server/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,9 +1,20 @@
+import { celebrate, Joi, Segments } from 'celebrate'
 import { Router } from 'express'
 import SessionsController from '../controllers/SessionsController'
 
 const sessionsRouter = Router()
 const sessionsController = new SessionsController()
 
-sessionsRouter.post('/', sessionsController.authenticate)
+sessionsRouter.post('/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.when('email', {
+        is: Joi.string(),
+        then: Joi.string().required()
+      })
+    }
+  })
+,sessionsController.authenticate)
 
 export default sessionsRouter
