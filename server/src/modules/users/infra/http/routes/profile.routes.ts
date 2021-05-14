47141174c1f07c0630313f66ec2c6ee69942c7d2
x14102004x <marcosproenca144@gmail.com>
2021-05-14 19:40:03 -0300

    feat: the celebrate validation middleware was added in profile

--- server/src/modules/users/infra/http/routes/profile.routes.ts
+++ server/src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,3 +1,4 @@
+import { celebrate, Joi, Segments } from 'celebrate'
 import { Router } from 'express'
 import ProfileController from '../controllers/ProfileController'
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
@@ -8,6 +9,20 @@ const profileController = new ProfileController()
 profileRouter.use(ensureAuthenticated)
 
 profileRouter.get('/', profileController.show)
-profileRouter.put('/', profileController.update)
+profileRouter.put('/', celebrate({
+  [Segments.BODY]: {
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    old_password: Joi.string(),
+    password: Joi.when('old_password', {
+      is: Joi.exist(),
+      then: Joi.string().required()
+    }),
+    password_confirmation: Joi.when('password', {
+      is: Joi.exist(),
+      then: Joi.string().required().valid(Joi.ref('password'))
+    })
+  }
+}),profileController.update)
 
 export default profileRouter
