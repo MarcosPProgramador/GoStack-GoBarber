47046aea387673d22f43769e15038c32e23915c8
x14102004x <marcosproenca144@gmail.com>
2021-05-16 19:20:15 -0300

    feat: the file where all routes are

---
+++ server/src/shared/infra/http/routes/index.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes'
+import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes'
+import usersRouter from '@modules/users/infra/http/routes/users.routes'
+import passwordRouter from '@modules/users/infra/http/routes/password.routes'
+import profileRouter from '@modules/users/infra/http/routes/profile.routes'
+import providersRouter from '@modules/appointments/infra/http/routes/providers.routes'
+
+const router = Router()
+
+router.use('/appointments', appointmentsRouter)
+router.use('/providers', providersRouter)
+router.use('/users', usersRouter)
+router.use('/sessions', sessionsRouter)
+router.use('/password', passwordRouter)
+router.use('/profile', profileRouter)
+
+export default router
