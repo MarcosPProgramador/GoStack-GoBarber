530847252e6b87aa34b885e107ac9b4fa492984b
x14102004x <marcosproenca144@gmail.com>
2021-05-15 21:53:37 -0300

    refactor: more settings in the upload file have been added

--- server/src/config/upload.ts
+++ server/src/config/upload.ts
@@ -1,21 +1,44 @@
 import crypto from 'crypto'
-import multer from 'multer'
+import multer, {StorageEngine} from 'multer'
 import path from 'path'
-
+interface IUploadsConfig {
+  driver: 'disk' | 's3'
+  tmpFolder: string,
+  uploadsFolder: string
+  multer: {
+    storage: StorageEngine
+  }
+  config: {
+    disk: {}
+    aws: {
+      bucket: string
+    }
+  }
+}
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp')
 
 export default {
+  driver: process.env.STORAGE_DRIVER || 'disk',
   tmpFolder,
   uploadsFolder: path.resolve(tmpFolder, 'uploads'),
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename: (request, file, callback) => {
-      const hash = crypto.randomBytes(10).toString('hex')
-      const originalName = file.originalname.replace(' ', '-')
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename: (request, file, callback) => {
+        const hash = crypto.randomBytes(10).toString('hex')
+        const originalName = file.originalname.replace(' ', '-')
 
-      const fileName = `${hash}-${originalName}`
+        const fileName = `${hash}-${originalName}`
 
-      callback(null, fileName)
-    },
-  }),
-}
+        callback(null, fileName)
+      },
+    }),
+
+  },
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'aws'
+    }
+  }
+} as IUploadsConfig
