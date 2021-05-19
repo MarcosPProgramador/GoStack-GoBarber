63a9f72e23be04e212ed26c886a094180581dfb3
x14102004x <marcosproenca144@gmail.com>
2021-05-19 20:31:01 -0300

    test: the fake-cache provider was included and used in the appointments list file

--- server/src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
+++ server/src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -1,12 +1,19 @@
+import FakeCacheProvider from "@shared/container/providers/CacheProvider/fakes/FakeCacheProvider"
 import FakeAppointmentsRepository from "../repositories/fakes/FakeAppointmentsRepository"
 import ListProviderAppointmentsService from "./ListProviderAppointmentsService"
 
 let listProviderAppointments: ListProviderAppointmentsService
 let fakeAppointmentsRepository: FakeAppointmentsRepository
+let fakeCacheProvider: FakeCacheProvider
+
 describe('ListProviderAppointments', () => {
   beforeEach(() => {
     fakeAppointmentsRepository = new FakeAppointmentsRepository()
-    listProviderAppointments = new  ListProviderAppointmentsService(fakeAppointmentsRepository)
+    fakeCacheProvider = new FakeCacheProvider()
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+      fakeCacheProvider
+    )
   })
   it('should be able to list all appointments from a provider', async () => {
     const appointment1 = await fakeAppointmentsRepository.create({
@@ -26,5 +33,33 @@ describe('ListProviderAppointments', () => {
       year: 2021
     })
     expect(appointments).toEqual([appointment1, appointment2])
+  })
+  it('should be able to list all appointments from a provider by cache provider', async () => {
+    const primaryAppointment = await fakeAppointmentsRepository.create({
+      provider_id: 'provider-id',
+      user_id: 'user-id',
+      date: new Date(2021, 4, 12, 13)
+    })
+    const secondaryAppointment = await fakeAppointmentsRepository.create({
+      provider_id: 'provider-id',
+      user_id: 'user-id',
+      date: new Date(2021, 4, 12, 13)
+    })
+    const appointments = await listProviderAppointments.execute({
+      provider_id: 'provider-id',
+      day: 12,
+      month: 5,
+      year: 2021
+    })
+    await listProviderAppointments.execute({
+      provider_id: 'provider-id',
+      day: 12,
+      month: 5,
+      year: 2021
+    })
+
+
+    expect(appointments).toEqual([primaryAppointment, secondaryAppointment])
+
   })
 })
