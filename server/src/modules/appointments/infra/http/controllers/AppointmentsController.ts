43adc227dcc493636fa08e095abaacc8be8446fa
x14102004x <marcosproenca144@gmail.com>
2021-05-19 20:27:25 -0300

    refactor: the date-fns parse-iso method has been removed due to lack of need

--- server/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ server/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,5 +1,4 @@
 import CreateAppointmentService from "@modules/appointments/services/CreateAppointmentService"
-import { parseISO } from "date-fns"
 import { Request, Response } from "express"
 import { container } from "tsyringe"
 
@@ -8,13 +7,11 @@ class AppointmentsController {
     const { provider_id, date } = request.body
     const user_id = request.user.id
 
-    const parsedDate = parseISO(date)
-
     const createAppointmentService = container.resolve(CreateAppointmentService)
     const appointment = await createAppointmentService.execute({
       provider_id,
       user_id,
-      date: parsedDate,
+      date,
     })
 
     return response.status(201).json(appointment)
