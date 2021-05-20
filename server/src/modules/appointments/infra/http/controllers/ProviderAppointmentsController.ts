758acf1cdb2f0945d92e546a4e4f0cfdbed130e8
x14102004x <marcosproenca144@gmail.com>
2021-05-20 12:36:28 -0300

    refactor: the field names are now being sent by query params

--- server/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ server/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -1,20 +1,21 @@
 import ListProviderAppointmentsService from "@modules/appointments/services/ListProviderAppointmentsService"
+import { classToClass } from "class-transformer"
 import { Request, Response } from "express"
 import { container } from "tsyringe"
 
 export default class ProviderAppointmentsController {
   async index(request: Request, response: Response) : Promise<Response>  {
     const provider_id = request.user.id
-    const { day, month, year } = request.body
+    const { day, month, year } = request.query
 
     const listProviderAppointments = container.resolve(ListProviderAppointmentsService)
 
     const appointments = await listProviderAppointments.execute({
       provider_id,
-      day,
-      month,
-      year
+      year: Number(year),
+      month: Number(month),
+      day: Number(day),
     })
-    return response.status(200).json(appointments)
+    return response.status(200).json(classToClass(appointments))
   }
 }
