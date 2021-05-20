7ac89c09a257239e1a17865a94200069fdb792e8
x14102004x <marcosproenca144@gmail.com>
2021-05-20 12:41:06 -0300

    refactor: the field names are no longer sent by body

--- server/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
+++ server/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -5,15 +5,15 @@ import { container } from "tsyringe";
 export default class ProviderDayAvailabilityController {
   async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params
-    const { month, year, day } = request.body
+    const { month, year, day } = request.query
 
     const listProviderDayAvailability = container.resolve(ListProviderDayAvailabilityService)
 
     const availability = await listProviderDayAvailability.execute({
       provider_id,
-      month,
-      year,
-      day
+      year: Number(year),
+      month: Number(month),
+      day: Number(day),
     })
 
     return response.status(200).json(availability)
