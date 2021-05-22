c28d02ce23b1999a1a8ea16974f2119b4562f6fd
x14102004x <marcosproenca144@gmail.com>
2021-05-22 15:55:23 -0300

    test: the cache provider was added to the list-providers test

--- server/src/modules/appointments/services/ListProvidersService.spec.ts
+++ server/src/modules/appointments/services/ListProvidersService.spec.ts
@@ -1,14 +1,17 @@
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository'
+import FakeCacheProvider from '@shared/container/providers/CacheProvider/fakes/FakeCacheProvider'
 import ListProvidersService from './ListProvidersService'
 
 let fakeUsersRepository: FakeUsersRepository
 let listProviders: ListProvidersService
-
+let fakeCacheProvider: FakeCacheProvider
 describe('ListProviders', () => {
   beforeEach(() => {
     fakeUsersRepository = new FakeUsersRepository()
+    fakeCacheProvider = new FakeCacheProvider()
     listProviders = new ListProvidersService(
-      fakeUsersRepository
+      fakeUsersRepository,
+      fakeCacheProvider
     )
   })
   it('should be able to list all providers except the user', async () => {
@@ -30,6 +33,33 @@ describe('ListProviders', () => {
     })
 
     const providers = await listProviders.execute({
+      user_id: loggedUser.id
+    })
+
+    expect(providers).toEqual([primaryUser, secondaryUser])
+  })
+  it('should be able to list all providers except the user by cache provider', async () => {
+    const primaryUser = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456'
+    })
+    const secondaryUser = await fakeUsersRepository.create({
+      name: 'John thr',
+      email: 'johnthr@example.com',
+      password: '123456'
+    })
+
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'John Fou',
+      email: 'johnfou@example.com',
+      password: '123456'
+    })
+
+    const providers = await listProviders.execute({
+      user_id: loggedUser.id
+    })
+    await listProviders.execute({
       user_id: loggedUser.id
     })
 
