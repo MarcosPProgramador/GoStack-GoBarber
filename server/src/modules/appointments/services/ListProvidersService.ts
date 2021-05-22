10eba35bad38126755e7d255c916663ef958bb93
x14102004x <marcosproenca144@gmail.com>
2021-05-22 16:08:12 -0300

    feat: the cache provider was injected respecting the dip and lsp

--- server/src/modules/appointments/services/ListProvidersService.ts
+++ server/src/modules/appointments/services/ListProvidersService.ts
@@ -1,5 +1,7 @@
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import { inject, injectable } from 'tsyringe'
 
 interface IRequest {
@@ -10,12 +12,20 @@ interface IRequest {
 export default class FindProvidersService {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider
   ) { }
   async execute({ user_id }: IRequest): Promise<User[]> {
-    const providers = await this.usersRepository.findAllProviders({
-      except_user_id: user_id
-    })
+    let providers = await this.cacheProvider.recover<User[]>(`providers-list:${user_id}`)
+
+    if(!providers) {
+      providers = await this.usersRepository.findAllProviders({
+        except_user_id: user_id
+      })
+      await this.cacheProvider.save(`providers-list:${user_id}`, classToClass(providers))
+    }
     return providers
   }
 }
