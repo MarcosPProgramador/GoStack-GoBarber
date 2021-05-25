9ddb457c59fcafbc5bebd5c23d636b0a47ac41f3
x14102004x <marcosproenca144@gmail.com>
2021-05-25 12:52:53 -0300

    feat: scrollbar style was added in the global-style file

--- web/src/styles/globalStyle.ts
+++ web/src/styles/globalStyle.ts
@@ -1,62 +1,82 @@
 import { createGlobalStyle } from 'styled-components';
+
 export default createGlobalStyle`
-    *{
-        margin: 0;
-        padding: 0;
-        outline: 0;
-        appearance: none;
-        box-sizing: border-box;        
-    }    
-    html, body, #app {
-        width: 100%;
-        min-height: 100vh;
-        min-width: 260px;
-        overflow: hidden;
-      }
-    #app {
-      background-color: ${props => props.theme.colors.background};
-      color: ${props => props.theme.colors.text};
-      font-family: 'Roboto Slab', sans-serif;
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    appearance: none;
+    box-sizing: border-box;
+    scrollbar-color: ${({ theme: { colors } }) =>
+      `${colors.shape} ${colors.medium}`};
+    &::-webkit-scrollbar {
+      width: 15px;
+      background-color: ${props => props.theme.colors.medium};
     }
-    h1,h2,h3,h4,h5,h6,strong {
-      font-weight: 500;
-      font-size: 16px;
+    &::-webkit-scrollbar-thumb {
+      border-radius: 50rem;
+      background-color: ${props => props.theme.colors.shape};
     }
-    ul {
-      list-style: none;
-    }
-    a{
-      text-decoration: none;
-    }
-    input::placeholder {
-      font-weight: 400;
-      color: ${props => props.theme.colors.hard}; 
-    }
-    
-    input:-webkit-autofill,
-    input:-webkit-autofill:hover,
-    input:-webkit-autofill:focus,
-    input:-webkit-autofill:active {
-      box-shadow: 0 0 0 30px ${props => props.theme.colors.inputs} inset !important;
-    }
-    input:-webkit-autofill {
-      -webkit-text-fill-color: ${props => props.theme.colors.text} !important;
-    }
-    a, button, input {
-      border-radius: 0;
-      font-family: 'Roboto Slab', sans-serif;
-      font-weight: 400;
-      border: 0;
-      background-color: transparent;
-      color: ${props => props.theme.colors.text}; 
-    }
+  }
+  html,
+  body,
+  #app {
+    width: 100%;
+    min-height: 100vh;
+    min-width: 260px;
+    overflow: hidden;
+  }
+  #app {
+    background-color: ${props => props.theme.colors.background};
+    color: ${props => props.theme.colors.text};
+    font-family: 'Roboto Slab', sans-serif;
+  }
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  strong {
+    font-weight: 500;
+    font-size: 16px;
+  }
+  ul {
+    list-style: none;
+  }
+  a {
+    text-decoration: none;
+  }
+  input::placeholder {
+    font-weight: 400;
+    color: ${props => props.theme.colors.hard};
+  }
 
-    button {
-      cursor: pointer;
-      *{
-        pointer-events: none;
-      }
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus,
+  input:-webkit-autofill:active {
+    box-shadow: 0 0 0 30px ${props =>
+      props.theme.colors.inputs} inset !important;
+  }
+  input:-webkit-autofill {
+    -webkit-text-fill-color: ${props => props.theme.colors.text} !important;
+  }
+  a,
+  button,
+  input {
+    border-radius: 0;
+    font-family: 'Roboto Slab', sans-serif;
+    font-weight: 400;
+    border: 0;
+    background-color: transparent;
+    color: ${props => props.theme.colors.text};
+  }
+
+  button {
+    cursor: pointer;
+    * {
+      pointer-events: none;
     }
-
-`
-
+  }
+`;
