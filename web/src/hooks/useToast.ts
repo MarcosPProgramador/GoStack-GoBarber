863ff8a4920321eb43c26f8bc84e50f1de39bf8d
x14102004x <marcosproenca144@gmail.com>
2021-05-25 11:56:17 -0300

    feat: the hook that is using the context that creates a toast

--- web/src/hooks/useToast.ts
+++ web/src/hooks/useToast.ts
@@ -1,12 +1,12 @@
-import { useContext } from "react"
-import { IToastContext, ToastContext } from "../contexts/ToastProvider"
+import { useContext } from 'react';
+import { IToastContext, ToastContext } from '../contexts/ToastProvider';
 
 function useToast(): IToastContext {
-  const context = useContext(ToastContext)
+  const context = useContext(ToastContext);
 
-  if(!context) throw new Error('useToast must be used within a ToastProvider')
+  if (!context) throw new Error('useToast must be used within a ToastProvider');
 
-  return context
+  return context;
 }
 
-export default useToast
+export default useToast;
