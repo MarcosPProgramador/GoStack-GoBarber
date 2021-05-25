41634bc29c47d4870b03ce6ae6dbbcdf6da8680f
x14102004x <marcosproenca144@gmail.com>
2021-05-25 12:02:26 -0300

    feat: the persistent hook of the dark and light theme was created

--- web/src/hooks/usePersistedState.ts
+++ web/src/hooks/usePersistedState.ts
@@ -1,12 +1,10 @@
-import { Dispatch, SetStateAction, useState } from "react"
-type IResponse<T> = [
-    T,
-    Dispatch<SetStateAction<T>>
-]
-function usePersistedState<T>(key: string, initialState: T): IResponse<T> {
-    const [state, setState] = useState(initialState)
+import { Dispatch, SetStateAction, useState } from 'react';
 
-    return [state, setState]
+type IResponse<T> = [T, Dispatch<SetStateAction<T>>];
+function usePersistedState<T>(key: string, initialState: T): IResponse<T> {
+  const [state, setState] = useState(initialState);
+
+  return [state, setState];
 }
 
-export default usePersistedState
+export default usePersistedState;
